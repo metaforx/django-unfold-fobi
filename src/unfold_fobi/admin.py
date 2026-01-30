1f0cc5814de0184d59bbed85910eceaf970108ee
Marc Widmer <marc@pbi.io>
2026-01-30 18:26:03 +0100

    feat: integrate custom views for FormEntry management in admin interface

--- src/unfold_fobi/admin.py
+++ src/unfold_fobi/admin.py
@@ -4,6 +4,7 @@ from django.urls import path, reverse
 from django.utils.html import format_html
 from unfold.admin import ModelAdmin
 from .models import FormEntryProxy
+from .views import FormEntryCreateView, FormEntryEditView, FormEntryImportView, FormWizardsDashboardView
 
 @admin.register(FormEntryProxy)
 class FormEntryProxyAdmin(ModelAdmin):
@@ -20,38 +21,35 @@ class FormEntryProxyAdmin(ModelAdmin):
     
     def name_link(self, obj):
         """Display name as link to custom edit view."""
-        edit_url = reverse('fobi.edit_form_entry', args=[obj.pk])
+        edit_url = reverse('admin:unfold_fobi_formentryproxy_edit', args=[obj.pk])
         return format_html('<a href="{}">{}</a>', edit_url, obj.name)
     name_link.short_description = 'Name'
     name_link.admin_order_field = 'name'
     
     def get_urls(self):
         """Add custom URLs for create, import, and wizards."""
-        from django.views.generic import RedirectView
-        from django.shortcuts import redirect
-        
         urls = super().get_urls()
-        
-        # Add redirect views for create, import, and wizards
-        # Use lambda to defer reverse() call until request time
-        def redirect_to_create(request):
-            return redirect('fobi.create_form_entry')
-        
-        def redirect_to_import(request):
-            return redirect('fobi.import_form_entry')
-        
-        def redirect_to_wizards(request):
-            return redirect('fobi.form_wizards_dashboard')
-        
+
         custom_urls = [
+            path('edit/<int:form_entry_id>/',
+                 self.admin_site.admin_view(
+                     FormEntryEditView.as_view(model_admin=self)
+                 ),
+                 name='%s_%s_edit' % (self.model._meta.app_label, self.model._meta.model_name)),
             path('create/', 
-                 self.admin_site.admin_view(redirect_to_create),  # Use admin_view to ensure proper processing
+                 self.admin_site.admin_view(
+                     FormEntryCreateView.as_view(model_admin=self)
+                 ),
                  name='%s_%s_create' % (self.model._meta.app_label, self.model._meta.model_name)),
             path('import/', 
-                 self.admin_site.admin_view(redirect_to_import),  # Use admin_view to ensure proper processing
+                 self.admin_site.admin_view(
+                     FormEntryImportView.as_view(model_admin=self)
+                 ),
                  name='%s_%s_import' % (self.model._meta.app_label, self.model._meta.model_name)),
             path('wizards/', 
-                 self.admin_site.admin_view(redirect_to_wizards),  # Use admin_view to ensure proper processing
+                 self.admin_site.admin_view(
+                     FormWizardsDashboardView.as_view(model_admin=self)
+                 ),
                  name='%s_%s_wizards' % (self.model._meta.app_label, self.model._meta.model_name)),
         ]
         
@@ -66,4 +64,4 @@ class FormEntryProxyAdmin(ModelAdmin):
     def response_change(self, request, obj):
         """Override to redirect to custom edit view after save."""
         from django.shortcuts import redirect
-        return redirect('fobi.edit_form_entry', form_entry_id=obj.pk)
+        return redirect('admin:unfold_fobi_formentryproxy_edit', form_entry_id=obj.pk)
