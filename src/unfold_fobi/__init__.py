f912e0f3a6d22298c1cab356305db1242f03eaa2
Marc Widmer <marc@pbi.io>
2026-03-28 16:02:52 +0100

    chore: bump version to 0.1.6

--- src/unfold_fobi/__init__.py
+++ src/unfold_fobi/__init__.py
@@ -2,7 +2,7 @@
 
 import warnings
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 # Python 3.12 emits SyntaxWarning for invalid escape sequences in older fobi
 # releases (fobi.base uses "\s" in non-raw strings). Suppress only this known
